be10999b74d4591363de657a796f0f0d9dd05361
victor.choi <victor.choi@me.com>
2021-04-11 10:29:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+[다운로드]()
+
+스트리트 파인더는 맥용 앱 알프레드 워크플로우입니다. 
+스트리트 파인더는 두 가지 기능을 제공합니다. 
+
+1. 구 주소를 이용해서 도로명 주소 검색 
+옛 주소 정보를 이용해서 도로명 주소를 검색할 수 있습니다.
+
+**사용법**
+알프레드 검색창에 다음과 같이 입력합니다.
+```
+sdoro {검색어}
+```
+
+검색 결과로 도로명 주소 목록이 나타나고 선택한 도로명 주소를 Command + C로 복사할 수 있습니다. 
+
+2. 한국어 도로명 주소를 영문 도로명 주소로 변환 
+한국어 도로명 주소를 영문 도로명 주소로 변환할 수 있습니다. 
+
+**사용법** 
+```
+sdoroe {검색어}
+```
+
+검색 결과로 영문 도로명 주소 목록이 나타나고 선택한 도로명 주소를 Command + C로 복사할 수 있습니다. 
+
+---------------------------------------------------------------------------------
+
+Street finder is an [Alfred workflow](https://www.alfredapp.com). 
+Street finder has two features. 
+
+1. Find a street address in Korean 
+You can search a new street address in Korean with an old street nam or a keyword. 
+
+```
+sdoro {keyword}
+```
+
+As a result, you will see the list of addresses and you can copy one of them with command + c. 
+
+2. Convert Korean street name to English street name 
+You can convert a street name in Korean to a street name in English. 
+
+```
+sdoroe {keyword} 
+```
+
+As a result, you will see the list of addresses and you can copy one of them with command + c.
+
+
---
+++ app.py
@@ -0,0 +1,54 @@
+# -*- coding: utf8 -*-
+from urllib import quote_plus, urlencode, quote
+import urllib2
+import sys 
+import json 
+import ast
+from urlparse import urlparse, parse_qs
+import unicodedata 
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+keyword = u"{query}"
+keyword = unicodedata.normalize('NFC', keyword)
+
+queryParams = urlencode({ quote_plus('currentPage') : '1' , quote_plus('countPerPage') : '10', quote_plus('resultType') : 'json', quote_plus('keyword') : keyword, quote_plus('confmKey') : 'U01TX0FVVEgyMDIxMDQwOTEwMTMzOTExMTAyNzA='})
+
+url = "https://www.juso.go.kr/addrlink/addrEngApi.do?" + queryParams
+def getAddrItemForAlfred(item, keyword):
+	result = { 
+		"type": "file:skipcheck",
+		"title": item["roadAddr"],
+		"subtitle": item["roadAddr"] + ' ' + item["zipNo"],
+		"arg": item["roadAddr"] + ' ' + item["zipNo"],
+		"autocomplete": item["roadAddr"] + ' ' + item["zipNo"],
+		"text": {
+			"copy": item["roadAddr"] + ' ' + item["zipNo"],
+			"largetype": item["roadAddr"] + ' ' + item["zipNo"],
+		}, 
+		"quicklookurl": item["roadAddr"] + ' ' + item["zipNo"],
+	}	
+	return result
+
+def app():
+	results = []
+	if len(keyword) > 0:
+		response = urllib2.urlopen(url)
+		jsonData = response.read()
+		# jsonData = str(response.read())
+		d = eval(jsonData)
+		addrs = d["results"]["juso"]
+		for item in addrs:
+			if len(results) == 0:
+				results = [getAddrItemForAlfred(item, keyword)]
+			else:
+				results.append(getAddrItemForAlfred(item, keyword))
+
+		final = json.dumps({	"items": results })
+	else:
+		final = json.dumps({	"items": results })
+
+	print(final)
+
+app() 
